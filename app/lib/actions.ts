4440a0baa0e0aa94da4600f844fe1583862c1791
Kántor István <kantoristvan17@gmail.com>
2025-04-09 13:38:42 +0200

    not-found

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -64,10 +64,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  try {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath("/dashboard/invoices");
-  } catch (error) {
-    console.error("Error deleting invoice:", error);
-  }
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
 }
